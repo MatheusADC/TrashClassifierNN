086cfb5a7ec3d3bb4f00668f2a840718f2125c50
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-03-22 02:59:19 -0300

    Add: Server file

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const multer = require("multer");
+const tf = require("@tensorflow/tfjs-node");
+const path = require("path");
+const app = express();
+const port = YOUR_PORT;
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+const modelPath = path.join(__dirname, "model");
+
+let model;
+
+async function loadModel() {
+    try {
+        model = await tf.loadGraphModel(`file://${modelPath}/model.json`);
+        console.log("Model loaded!");
+    } catch (error) {
+        console.error("Error loading the model:", error);
+    }
+}
+
+loadModel();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/model", express.static(modelPath));
+
+app.post("/classify", upload.single("image"), async (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: "No image uploaded" });
+    }
+
+    try {
+        const imageBuffer = req.file.buffer;
+        const tensor = tf.node.decodeImage(imageBuffer).expandDims(0);
+
+        if (!model) {
+            return res.status(500).json({ error: "Model not loaded correctly" });
+        }
+
+        const predictions = await model.predict(tensor);
+
+        const results = predictions.map((p) => ({
+            className: p.className,
+            probability: (p.probability * 100).toFixed(2) + "%",
+        }));
+
+        res.json({ predictions: results });
+    } catch (error) {
+        console.error("Erro ao classificar imagem:", error);
+        res.status(500).json({ error: "Erro ao processar a imagem" });
+    }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
